cb5be2c3e8c803cbf60ca4af5aefa1e5dd2b0206
Andrew Shalimov <ashalimov@acceptic.net>
2018-04-18 09:26:02 +0300

    remove unused methods

--- src/main/java/com/ictcg/binance/client/WSClient.java
+++ src/main/java/com/ictcg/binance/client/WSClient.java
@@ -9,47 +9,7 @@ import java.net.URI;
 @ClientEndpoint
 public class WSClient {
 
-    private static Object waitLock = new Object();
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @OnMessage
-    public void onMessage(String message) {
-        logger.info("Received msg: {}", message);
-    }
-
-    public WSClient() {
-        logger.info("created");
-
-    }
-
-    private void wait4TerminateSignal() {
-        synchronized (waitLock) {
-            try {
-                waitLock.wait();
-            } catch (InterruptedException e) {
-            }
-        }
-    }
-
-    public void connect_(String url) {
-        WebSocketContainer container = null;//
-        Session session = null;
-        try {
-            container = ContainerProvider.getWebSocketContainer();
-            session = container.connectToServer(WSClient.class, URI.create(url));
-            wait4TerminateSignal();
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        } finally {
-            if (session != null) {
-                try {
-                    session.close();
-                } catch (Exception e) {
-                    logger.error(e.getMessage());
-                }
-            }
-        }
-    }
 
     public void subscribe(String url) {
         subscribe(url, null);
